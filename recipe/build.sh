a361a87e6971bd56ddcfde9d00d61d8e02f408df
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 20:45:49 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/influx
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/influx
 go-licenses save ./cmd/influx --save_path=license-files
